3fdf24de45f1a4fdbbb8906dfa2a5d9c72f3c75d
Nikita <yazevnikita441@gmail.com>
2026-09-20 18:21:58 +0300

    Add intentional logical error

--- calculator.py
+++ calculator.py
@@ -31,7 +31,7 @@ def clear_display():
 def calculate():
     try:
         expression = display.get()
-        result = eval(expression)
+        result = eval(expression) + 1
 
         display.delete(0, tk.END)
         display.insert(0, result)
